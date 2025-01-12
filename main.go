a6aad550c90cfcb5cf6e1d1307ab9320e06bbc71
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-01-12 15:24:31 +0000

    fix: add NewProducer function, rename config names

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-				"net"
-				"io"
-				"log/slog"
+	"kafka_rewrite/producer"
 )
 
+func NewProducer(config *producer.Config, conn *producer.Connection) *producer.Producer {
+			return &producer.Producer{
+							config:  config ,
+							conn: conn}
 
-type Server struct {
-				ln net.Listener
-}
-
-func (s *Server) Start() error {
-				return nil
-}
-
-func (s* Server) listen() error {
-				ln , err := net.Listen("tcp", ":9092")
-				if err != nil {
-								return err
-				}
-				s.ln = ln
-				for {
-								conn, err := ln.Accept()
-								if err != nil {
-												if err == io.EOF {
-																return err
-												}
-												slog.Error("server accept error", "err", err)
-								}
-								go s.handleConn(conn)
-				}
 }
