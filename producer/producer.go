ead7935c3b3bc0be03c0ba050edbf319b19bafa2
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-01-14 20:02:54 +0000

    feat(producer): add producer pointed WriteMessage function

--- producer/producer.go
+++ producer/producer.go
@@ -1,6 +1,14 @@
 package producer
 
+import "kafka_rewrite/utils"
+
 type Producer struct {
-				Conf *Config
-				Conn *Connection
+	Conf *Config
+	Conn *Connection
+}
+
+func (producer *Producer) WriteMessage(message []byte) error {
+	config := producer.Conf
+	topic32 := utils.HashStringToInt32(config.Topic)
+	return producer.Conn.WriteMessage(topic32, message)
 }
