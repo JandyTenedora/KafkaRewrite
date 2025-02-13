df688fc7a23b91a0c4db596727b7dd599ae7fce6
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-02-13 20:38:18 +0000

    feat(broker.go): adding NewBroker function

--- broker/broker.go
+++ broker/broker.go
@@ -7,3 +7,13 @@ type Broker struct {
 	Topics      map[string][]byte
 	Connections []net.Conn
 }
+
+func NewBroker(address string) *Broker{
+				topics := make(map[string][]byte)
+				connections := make([]net.Conn, 3)
+				return &Broker{
+								Address: address,
+								Topics: topics,
+								Connections: connections,
+				}
+}
