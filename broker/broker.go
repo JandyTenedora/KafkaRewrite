7307ffbb9a7e514396062507d4fc0f0467cede96
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-02-14 15:52:36 +0000

    feat(broker.go): add Start() method

--- broker/broker.go
+++ broker/broker.go
@@ -1,6 +1,10 @@
 package broker
 
-import "net"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 type Broker struct {
 	Address     string
@@ -15,5 +19,25 @@ func NewBroker(address string) *Broker {
 		Address:     address,
 		Topics:      topics,
 		Connections: connections,
+	}
+}
+
+func (broker *Broker) Start() {
+	listener, error := net.Listen("tcp", broker.Address)
+	if error != nil {
+		log.Fatalf("Error starting listener %v", error)
+	}
+	defer listener.Close()
+
+	fmt.Printf("Broker started on on %s\n", broker.Address)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		broker.Connections = append(broker.Connections, conn)
+		fmt.Printf("New connection from %s\n", conn.RemoteAddr())
 	}
 }
