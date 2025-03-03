8262cb14edae08137eac4e9eec9ba6ab1335bb11
JandyTenedora <35187380+JandyTenedora@users.noreply.github.com>
2025-03-03 12:47:21 +0000

    feat(broker): add ReceiveAck method

--- broker/ack.go
+++ broker/ack.go
@@ -6,10 +6,24 @@ import (
 	"net"
 )
 
-func (broker *Broker) SendAck(conn net.Conn) error{
-	_, err := conn.Write([]byte(fmt.Sprintf("Message received from address: %v", broker.Address)))
+func (broker *Broker) SendAck(conn net.Conn) error {
+	_, err := conn.Write([]byte("ACK"))
 	if err != nil {
 		log.Fatalf("Error in sending ack: %v", err)
 	}
 	return err
+}
+
+func (broker *Broker) ReceiveAck(conn net.Conn) error {
+	buffer := make([]byte, 3)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		log.Fatalf("Error in receiving ack: %v", err)
+	}
+	ack := string(buffer)
+	if ack != "ACK" {
+		return fmt.Errorf("invalid ack: %s", ack)
+	}
+	log.Println("Received ack")
+	return nil
 }
